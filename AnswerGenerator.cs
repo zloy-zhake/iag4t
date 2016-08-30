4573800668ef7593e39def93e532ddd6c7b3f240
zloy-zhake <jk-jdogg@yandex.ru>
2016-08-31 00:35:52 +0600

    Добавил метод разбора входного файла в массив

--- AnswerGenerator.cs
+++ AnswerGenerator.cs
@@ -1,11 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace iag4t
 {
 	public class AnswerGenerator
 	{
-		public AnswerGenerator()
+		private string input_file;
+		private string output_file;
+		private int answers_count;
+		List<string[]> input_lines;
+
+		public AnswerGenerator(string in_f,
+		                       string out_f,
+		                       int n_ans)
 		{
+			this.input_file = in_f;
+			this.output_file = out_f;
+			this.answers_count = n_ans;
+			this.input_lines = new List<string[]>();
+		}
+
+		public void Read_From_File()
+		{
+			// прочитать строки из файла в массив строк input_lines[][2]
+			//{
+			//	Q1,A1;
+			// ...
+			//	Qn,An;
+			//}
+			// если в процессе чтения встречена ошибка форматирования, вывести сообщение (номер строки, текст на это строке)
+
+			string[] tmp_str_arr = File.ReadAllLines(this.input_file);
+
+			for(int i = 0; i < tmp_str_arr.Length; i + 2)
+			{
+				this.input_lines.Add(new string[2] { tmp_str_arr[i], tmp_str_arr[i + 1] });
+			}
 		}
 	}
 }
